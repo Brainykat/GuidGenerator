60e59e589fae3ee8c18c9b44427eceb85b6302a7
Chris <eng.christhuku@gmail.com>
2021-01-07 12:53:32 +0300

    Add project files.

---
+++ GuidGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GuidGenerator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello Chris!");
+      for (int i = 0; i < 5; i++)
+      {
+        Guid guid = Guid.NewGuid();
+        Console.WriteLine(guid.ToString().ToUpper());
+      }
+    }
+  }
+}
